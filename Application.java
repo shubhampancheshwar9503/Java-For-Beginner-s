223ec0f7bd5972dd755310d087d3b52409d361b6
shubhampancheshwar9503 <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-01-10 00:01:28 +0530

    Add files via upload

--- Application.java
+++ Application.java
@@ -2,12 +2,8 @@ public class Application {
 
     public static void main(String[] args) {
 
-        int value = 0;
-
-        while (value < 10) {
-            System.out.println("Hello " + value);
-
-            value = value + 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("The value of i is: %d\n", i);
         }
 
     }
