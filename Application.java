1abdddb5e7bdd67cfe5794c05513f29e5ccfd7c1
shubhampancheshwar9503 <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-01-11 01:46:54 +0530

    Add files via upload

--- Application.java
+++ Application.java
@@ -1,11 +1,27 @@
 public class Application {
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		// Some useful conditions:
+		System.out.println(5 == 5);
+		System.out.println(10 != 11);
+		System.out.println(3 < 6);
+		System.out.println(10 > 100);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("The value of i is: %d\n", i);
-        }
+		// Using loops with "break":
+		int loop = 0;
 
-    }
+		while (true) {
+			System.out.println("Looping: " + loop);
+
+			if (loop == 3) {
+				break;
+			}
+
+			loop++;
+
+			System.out.println("Running");
+		}
+
+	}
 
 }
