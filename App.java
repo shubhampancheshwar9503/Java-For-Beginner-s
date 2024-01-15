6031bc6f0b837521f909bc629f55cd674fd84e0c
shubhampancheshwar9503 <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-01-15 13:50:51 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,28 +1,32 @@
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int value = 7;
 
-        /*
-         * System.out.println("Enter a number: ");
-         * int value = scanner.nextInt();
-         * 
-         * while(value != 5) {
-         * System.out.println("Enter a number: ");
-         * value = scanner.nextInt();
-         * }
-         */
+        int[] values;
+        values = new int[3];
 
-        int value = 0;
-        do {
-            System.out.println("Enter a number: ");
-            value = scanner.nextInt();
-        } while (value != 5);
+        System.out.println(values[0]);
 
-        System.out.println("Got 5!");
+        values[0] = 10;
+        values[1] = 20;
+        values[2] = 30;
+
+        System.out.println(values[0]);
+        System.out.println(values[1]);
+        System.out.println(values[2]);
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+        }
+
+        int[] numbers = { 5, 6, 7 };
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
     }
 
 }
