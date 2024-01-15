29b74c6e955b85e159f0e55c54028c30e3f2ff4d
shubhampancheshwar9503 <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-01-16 00:54:41 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -2,31 +2,43 @@ public class App {
 
     public static void main(String[] args) {
 
-        int value = 7;
+        // Declare array of (references to) strings.
+        String[] words = new String[3];
 
-        int[] values;
-        values = new int[3];
+        // Set the array elements (point the references
+        // at strings)
+        words[0] = "Hello";
+        words[1] = "to";
+        words[2] = "you";
 
-        System.out.println(values[0]);
+        // Access an array element and print it.
+        System.out.println(words[2]);
 
-        values[0] = 10;
-        values[1] = 20;
-        values[2] = 30;
+        // Simultaneously declare and initialize an array of strings
+        String[] fruits = { "apple", "banana", "pear", "kiwi" };
 
-        System.out.println(values[0]);
-        System.out.println(values[1]);
-        System.out.println(values[2]);
-
-        for (int i = 0; i < values.length; i++) {
-            System.out.println(values[i]);
+        // Iterate through an array
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
 
-        int[] numbers = { 5, 6, 7 };
+        // "Default" value for an integer
+        int value = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        // Default value for a reference is "null"
+        String text = null;
 
+        System.out.println(text);
+
+        // Declare an array of strings
+        String[] texts = new String[2];
+
+        // The references to strings in the array
+        // are initialized to null.
+        System.out.println(texts[0]);
+
+        // ... But of course we can set them to actual strings.
+        texts[0] = "one";
     }
 
 }
