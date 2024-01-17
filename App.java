5eaea07df15160b2aa9763b104b760de0dd34a0e
shubhampancheshwar9503 <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-01-17 23:31:39 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,44 +1,56 @@
 public class App {
-
+ 
     public static void main(String[] args) {
-
-        // Declare array of (references to) strings.
-        String[] words = new String[3];
-
-        // Set the array elements (point the references
-        // at strings)
-        words[0] = "Hello";
-        words[1] = "to";
-        words[2] = "you";
-
-        // Access an array element and print it.
-        System.out.println(words[2]);
-
-        // Simultaneously declare and initialize an array of strings
-        String[] fruits = { "apple", "banana", "pear", "kiwi" };
-
-        // Iterate through an array
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+         
+        // 1D array
+        int[] values = {3, 5, 2343};
+         
+        // Only need 1 index to access values.
+        System.out.println(values[2]);
+         
+        // 2D array (grid or table)
+        int[][] grid = {
+            {3, 5, 2343},
+            {2, 4},
+            {1, 2, 3, 4}
+        };
+         
+        // Need 2 indices to access values
+        System.out.println(grid[1][1]);
+        System.out.println(grid[0][2]);
+         
+        // Can also create without initializing.
+        String[][] texts = new String[2][3];
+         
+        texts[0][1] = "Hello there";
+         
+        System.out.println(texts[0][1]);
+         
+        // How to iterate through 2D arrays.
+        // first iterate through rows, then for each row
+        // go through the columns.
+        for(int row=0; row < grid.length; row++) {
+            for(int col=0; col < grid[row].length; col++) {
+                System.out.print(grid[row][col] + "\t");
+            }
+             
+            System.out.println();
         }
-
-        // "Default" value for an integer
-        int value = 0;
-
-        // Default value for a reference is "null"
-        String text = null;
-
-        System.out.println(text);
-
-        // Declare an array of strings
-        String[] texts = new String[2];
-
-        // The references to strings in the array
-        // are initialized to null.
-        System.out.println(texts[0]);
-
-        // ... But of course we can set them to actual strings.
-        texts[0] = "one";
+         
+        // The last array index is optional.
+        String[][] words = new String[2][];
+         
+        // Each sub-array is null.
+        System.out.println(words[0]);
+         
+        // We can create the subarrays 'manually'.
+        words[0] = new String[3];
+         
+        // Can set a values in the sub-array we
+        // just created.
+        words[0][1] = "hi there";
+         
+        System.out.println(words[0][1]);
     }
-
+ 
 }
