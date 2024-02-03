0571b4244adf4c2b6e3cb492ccce6ed1951d13d9
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-03 12:28:48 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,22 +1,27 @@
-class Temp implements AutoCloseable {
-
-    @Override
-    public void close() throws Exception {
-        System.out.println("Closing!");
-        throw new Exception("oh no!");
-    }
-
-}
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
 
     public static void main(String[] args) {
 
-        try (Temp temp = new Temp()) {
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
 
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        map.put(5, "Five");
+        map.put(8, "Eight");
+        map.put(6, "Six");
+        map.put(4, "Four");
+        map.put(2, "Two");
+
+        String text = map.get(6);
+
+        System.out.println(text);
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+
+            System.out.println(key + ": " + value);
         }
 
     }
