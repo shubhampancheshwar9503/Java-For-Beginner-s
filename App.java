0240e69941e5634099eea663102b161be1539453
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-05 15:08:39 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,54 +1,66 @@
-class Thing {
-    public final static int LUCKY_NUMBER = 7;
-
-    public String name;
-    public static String description;
-
-    public static int count = 0;
-
-    public int id;
-
-    public Thing() {
-
-        id = count;
-
-        count++;
-    }
-
-    public void showName() {
-        System.out.println("Object id: " + id + ", " + description + ": " + name);
-    }
-
-    public static void showInfo() {
-        System.out.println(description);
-        // Won't work: System.out.println(name);
-    }
-}
-
 public class App {
 
     public static void main(String[] args) {
 
-        Thing.description = "I am a thing";
+        // Inefficient
+        String info = "";
 
-        Thing.showInfo();
+        info += "My name is Bob.";
+        info += " ";
+        info += "I am a builder.";
 
-        System.out.println("Before creating objects, count is: " + Thing.count);
+        System.out.println(info);
 
-        Thing thing1 = new Thing();
-        Thing thing2 = new Thing();
+        // More efficient.
+        StringBuilder sb = new StringBuilder("");
 
-        System.out.println("After creating objects, count is: " + Thing.count);
+        sb.append("My name is Sue.");
+        sb.append(" ");
+        sb.append("I am a lion tamer.");
 
-        thing1.name = "Bob";
-        thing2.name = "Sue";
+        System.out.println(sb.toString());
 
-        thing1.showName();
-        thing2.showName();
+        // The same as above, but nicer ....
 
-        System.out.println(Math.PI);
+        StringBuilder s = new StringBuilder();
 
-        System.out.println(Thing.LUCKY_NUMBER);
+        s.append("My name is Roger.")
+                .append(" ")
+                .append("I am a skydiver.");
+
+        System.out.println(s.toString());
+
+        ///// Formatting //////////////////////////////////
+
+        // Outputting newlines and tabs
+        System.out.print("Here is some text.\tThat was a tab.\nThat was a newline.");
+        System.out.println(" More text.");
+
+        // Formatting integers
+        // %-10d means: output an integer in a space ten characters wide,
+        // padding with space and left-aligning (%10d would right-align)
+        System.out.printf("Total cost %-10d; quantity is %d\n", 5, 120);
+
+        // Demo-ing integer and string formatting control sequences
+        for (int i = 0; i < 20; i++) {
+            System.out.printf("%-2d: %s\n", i, "here is some text");
+        }
+
+        // Formatting floating point value
+
+        // Two decimal place:
+        System.out.printf("Total value: %.2f\n", 5.6874);
+
+        // One decimal place, left-aligned in 6-character field:
+        System.out.printf("Total value: %-6.1f\n", 343.23423);
+
+        // You can also use the String.format() method if you want to retrieve
+        // a formatted string.
+        String formatted = String.format("This is a floating-point value: %.3f", 5.12345);
+        System.out.println(formatted);
+
+        // Use double %% for outputting a % sign.
+        System.out.printf("Giving it %d%% is physically impossible.", 100);
     }
 
 }
