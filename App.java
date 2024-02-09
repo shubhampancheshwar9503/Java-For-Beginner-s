13662f302c66bae1f2503162f708437a62815510
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-09 21:55:54 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,20 +1,34 @@
+import world.Plant;
+ 
+/*
+ * private --- only within same class
+ * public --- from anywhere
+ * protected -- same class, subclass, and same package
+ * no modifier -- same package only
+ */
+ 
 public class App {
-
+ 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-
-        // E.g. 4! = 4*3*2*1 (factorial 4)
-
-        System.out.println(factorial(5));
+        Plant plant = new Plant();
+         
+        System.out.println(plant.name);
+         
+        System.out.println(plant.ID);
+         
+        // Won't work --- type is private
+        //System.out.println(plant.type);
+         
+        // size is protected; App is not in the same package as Plant.
+        // Won't work
+        // System.out.println(plant.size);
+         
+        // Won't work; App and Plant in different packages, height has package-level visibility.
+        //System.out.println(plant.height);
+ 
     }
-
-    private static int factorial(int value) {
-        // System.out.println(value);
-
-        if (value == 1) {
-            return 1;
-        }
-
-        return factorial(value - 1) * value;
-    }
-
+ 
 }
