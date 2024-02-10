09d5efa75fbfa85ee07a218c9b0fb2f999230cc6
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-10 12:15:58 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,34 +1,54 @@
-import world.Plant;
- 
-/*
- * private --- only within same class
- * public --- from anywhere
- * protected -- same class, subclass, and same package
- * no modifier -- same package only
- */
- 
-public class App {
- 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        Plant plant = new Plant();
-         
-        System.out.println(plant.name);
-         
-        System.out.println(plant.ID);
-         
-        // Won't work --- type is private
-        //System.out.println(plant.type);
-         
-        // size is protected; App is not in the same package as Plant.
-        // Won't work
-        // System.out.println(plant.size);
-         
-        // Won't work; App and Plant in different packages, height has package-level visibility.
-        //System.out.println(plant.height);
- 
+class Thing {
+    public final static int LUCKY_NUMBER = 7;
+
+    public String name;
+    public static String description;
+
+    public static int count = 0;
+
+    public int id;
+
+    public Thing() {
+
+        id = count;
+
+        count++;
     }
- 
+
+    public void showName() {
+        System.out.println("Object id: " + id + ", " + description + ": " + name);
+    }
+
+    public static void showInfo() {
+        System.out.println(description);
+        // Won't work: System.out.println(name);
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Thing.description = "I am a thing";
+
+        Thing.showInfo();
+
+        System.out.println("Before creating objects, count is: " + Thing.count);
+
+        Thing thing1 = new Thing();
+        Thing thing2 = new Thing();
+
+        System.out.println("After creating objects, count is: " + Thing.count);
+
+        thing1.name = "Bob";
+        thing2.name = "Sue";
+
+        thing1.showName();
+        thing2.showName();
+
+        System.out.println(Math.PI);
+
+        System.out.println(Thing.LUCKY_NUMBER);
+    }
+
 }
