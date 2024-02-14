e6a22959405d468a666a6dbddb28d869e746541b
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-14 20:36:22 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,24 +1,66 @@
-class Temp implements AutoCloseable {
+import java.util.ArrayList;
+
+class Machine {
 
     @Override
-    public void close() throws Exception {
-        System.out.println("Closing!");
-        throw new Exception("oh no!");
+    public String toString() {
+        return "I am a machine";
     }
 
+    public void start() {
+        System.out.println("Machine starting.");
+    }
+
+}
+
+class Camera extends Machine {
+    @Override
+    public String toString() {
+        return "I am a camera";
+    }
+
+    public void snap() {
+        System.out.println("snap!");
+    }
 }
 
 public class App {
 
     public static void main(String[] args) {
 
-        try (Temp temp = new Temp()) {
+        ArrayList<Machine> list1 = new ArrayList<Machine>();
 
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        list1.add(new Machine());
+        list1.add(new Machine());
+
+        ArrayList<Camera> list2 = new ArrayList<Camera>();
+
+        list2.add(new Camera());
+        list2.add(new Camera());
+
+        showList(list2);
+        showList2(list1);
+        showList3(list1);
+    }
+
+    public static void showList(ArrayList<? extends Machine> list) {
+        for (Machine value : list) {
+            System.out.println(value);
+            value.start();
         }
 
+    }
+
+    public static void showList2(ArrayList<? super Camera> list) {
+        for (Object value : list) {
+            System.out.println(value);
+        }
+    }
+
+    public static void showList3(ArrayList<?> list) {
+        for (Object value : list) {
+            System.out.println(value);
+        }
     }
 
 }
