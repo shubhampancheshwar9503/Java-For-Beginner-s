6d390d7bf96fd8206fe760432a7ea714512a51c9
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-18 14:44:48 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,31 +1,11 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import ocean.Fish;
+import ocean.plants.Seaweed;
 
 public class App {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // String fileName = "C:/Users/John/Desktop/example.txt";
-        String fileName = "example.txt";
-
-        File textFile = new File(fileName);
-
-        Scanner in = new Scanner(textFile);
-
-        int value = in.nextInt();
-        System.out.println("Read value: " + value);
-
-        in.nextLine();
-
-        int count = 2;
-        while (in.hasNextLine()) {
-            String line = in.nextLine();
-
-            System.out.println(count + ": " + line);
-            count++;
-        }
-
-        in.close();
+    public static void main(String[] args) {
+        Fish fish = new Fish();
+        Seaweed weed = new Seaweed();
     }
 
 }
