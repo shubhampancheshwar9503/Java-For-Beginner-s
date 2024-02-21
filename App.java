6b8f940817881b2e00633666afc59fba03b9f0e0
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-21 23:56:42 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,44 +1,45 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+class Machine {
+    public void start() {
+        System.out.println("Machine started.");
+    }
+}
 
-class Animal {
+class Camera extends Machine {
+    public void start() {
+        System.out.println("Camera started.");
+    }
 
+    public void snap() {
+        System.out.println("Photo taken.");
+    }
 }
 
 public class App {
-
     public static void main(String[] args) {
 
-        /////////////////// Before Java 5 ////////////////////////
-        ArrayList list = new ArrayList();
+        Machine machine1 = new Machine();
+        Camera camera1 = new Camera();
 
-        list.add("apple");
-        list.add("banana");
-        list.add("orange");
+        machine1.start();
+        camera1.start();
+        camera1.snap();
 
-        String fruit = (String) list.get(1);
+        // Upcasting
+        Machine machine2 = camera1;
+        machine2.start();
+        // error: machine2.snap();
 
-        System.out.println(fruit);
+        // Downcasting
+        Machine machine3 = new Camera();
+        Camera camera2 = (Camera) machine3;
+        camera2.start();
+        camera2.snap();
 
-        /////////////// Modern style //////////////////////////////
-
-        ArrayList<String> strings = new ArrayList<String>();
-
-        strings.add("cat");
-        strings.add("dog");
-        strings.add("alligator");
-
-        String animal = strings.get(1);
-
-        System.out.println(animal);
-
-        ///////////// There can be more than one type argument ////////////////////
-
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-
-        //////////// Java 7 style /////////////////////////////////
-
-        ArrayList<Animal> someList = new ArrayList<>();
+        // Doesn't work --- runtime error.
+        Machine machine4 = new Machine();
+        // Camera camera3 = (Camera)machine4;
+        // camera3.start();
+        // camera3.snap();
     }
 
 }
