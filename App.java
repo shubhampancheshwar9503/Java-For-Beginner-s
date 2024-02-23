8d92a82df1c14e23ca7d91213c4afd63986fb02f
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-02-23 11:43:36 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,45 +1,25 @@
-class Machine {
-    public void start() {
-        System.out.println("Machine started.");
-    }
-}
-
-class Camera extends Machine {
-    public void start() {
-        System.out.println("Camera started.");
-    }
-
-    public void snap() {
-        System.out.println("Photo taken.");
-    }
-}
-
 public class App {
+
     public static void main(String[] args) {
 
-        Machine machine1 = new Machine();
-        Camera camera1 = new Camera();
+        // Null pointer exception ....
+        String text = null;
 
-        machine1.start();
-        camera1.start();
-        camera1.snap();
+        System.out.println(text.length());
 
-        // Upcasting
-        Machine machine2 = camera1;
-        machine2.start();
-        // error: machine2.snap();
+        // Arithmetic exception ... (divide by zero)
+        int value = 7 / 0;
 
-        // Downcasting
-        Machine machine3 = new Camera();
-        Camera camera2 = (Camera) machine3;
-        camera2.start();
-        camera2.snap();
+        // You can actually handle RuntimeExceptions if you want to;
+        // for example, here we handle an ArrayIndexOutOfBoundsException
+        String[] texts = { "one", "two", "three" };
 
-        // Doesn't work --- runtime error.
-        Machine machine4 = new Machine();
-        // Camera camera3 = (Camera)machine4;
-        // camera3.start();
-        // camera3.snap();
+        try {
+            System.out.println(texts[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.toString());
+        }
+
     }
 
 }
