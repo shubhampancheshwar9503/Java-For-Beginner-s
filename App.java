c33025de0e186fa5e3a3f3d673b7c051c88b49c3
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-03-05 16:00:48 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,31 +1,21 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) {
-        Map<Integer, String> hashMap = new HashMap<Integer, String>();
-        Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
-        Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+        File file = new File("test.txt");
 
-        testMap(treeMap);
-    }
-
-    public static void testMap(Map<Integer, String> map) {
-        map.put(9, "fox");
-        map.put(4, "cat");
-        map.put(8, "dog");
-        map.put(1, "giraffe");
-        map.put(0, "swan");
-        map.put(15, "bear");
-        map.put(6, "snake");
-
-        for (Integer key : map.keySet()) {
-            String value = map.get(key);
-
-            System.out.println(key + ": " + value);
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+            br.write("This is line one");
+            br.newLine();
+            br.write("This is line two");
+            br.newLine();
+            br.write("Last line.");
+        } catch (IOException e) {
+            System.out.println("Unable to read file " + file.toString());
         }
 
     }
