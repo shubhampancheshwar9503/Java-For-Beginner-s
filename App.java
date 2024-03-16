a42220cd6558629c84d9bde1d47deb615c83e7f9
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-03-16 22:33:50 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,57 +1,74 @@
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
 
 public class App {
 
-    public static String[] vehicles = { "ambulance", "helicopter", "lifeboat" };
-
-    public static String[][] drivers = {
-            { "Fred", "Sue", "Pete" },
-            { "Sue", "Richard", "Bob", "Fred" },
-            { "Pete", "Mary", "Bob" }, };
-
     public static void main(String[] args) {
 
-        Map<String, Set<String>> personnel = new HashMap<String, Set<String>>();
+        System.out.println(new Object());
 
-        for (int i = 0; i < vehicles.length; i++) {
-            String vehicle = vehicles[i];
-            String[] driversList = drivers[i];
+        Person person1 = new Person(5, "Bob");
+        Person person2 = new Person(5, "Bob");
 
-            Set<String> driverSet = new LinkedHashSet<String>();
+        System.out.println(person1.equals(person2));
 
-            for (String driver : driversList) {
-                driverSet.add(driver);
-            }
+        Double value1 = 7.2;
+        Double value2 = 7.2;
 
-            personnel.put(vehicle, driverSet);
-        }
+        System.out.println(value1.equals(value2));
 
-        { // Brackets just to scope driversList variable so can use again later
-          // Example usage
-            Set<String> driversList = personnel.get("helicopter");
+        Integer number1 = 6;
+        Integer number2 = 6;
 
-            for (String driver : driversList) {
-                System.out.println(driver);
-            }
-        }
+        System.out.println(number1.equals(number2));
 
-        // Iterate through whole thing
-        for (String vehicle : personnel.keySet()) {
-            System.out.print(vehicle);
-            System.out.print(": ");
-            Set<String> driversList = personnel.get(vehicle);
+        String text1 = "Hello";
+        String text2 = "Hellodfadf".substring(0, 5);
 
-            for (String driver : driversList) {
-                System.out.print(driver);
-                System.out.print(" ");
-            }
+        System.out.println(text2);
 
-            System.out.println();
-        }
-
+        System.out.println(text1.equals(text2));
     }
 
 }
