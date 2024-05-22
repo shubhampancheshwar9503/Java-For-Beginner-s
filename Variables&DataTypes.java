b826c8f16d1aa05e6d67df4e628e626797c5a1fe
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-22 23:17:42 +0530

    Add files via upload

---
+++ Variables&DataTypes.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("The Sum Of This Number Is : ");
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+        int sum = num1 + num2+ num3;
+        System.out.println(sum);
+    }
+
+}
