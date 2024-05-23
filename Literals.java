1aa2706158631b2cd76a0f089008023c6e565909
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-23 11:08:46 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class CWH_04_literals {
+
+    public static void main(String[] args) {
+
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666l;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Shubham";
+        System.out.println(str);
+    }
+
+}
