9d5cca5bba4361738ec2275bb07b993cdb59c7b5
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-24 13:22:46 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_05_TakingInput {
+
+    public static void main(String[] args) {
+
+        System.out.println("Taking Input From The User ");
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number 1");
+        // int a = sc.nextInt();
+        // float a = sc.nextFloat();
+        // System.out.println("Enter Number 2");
+        // int b = sc.nextInt();
+        // float b =sc.nextInt();
+        // int sum = a + b;
+        // float sum = a + b;
+        // System.out.println("The Sum Of These Numbers Is");
+        // System.out.println(sum);
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
+        // String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+
+}
