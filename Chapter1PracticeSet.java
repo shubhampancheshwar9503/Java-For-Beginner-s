67f6d587527e0e7c6cc9d8e980620019e15b3945
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-26 12:45:27 +0530

    Add files via upload

---
+++ Chapter1PracticeSet.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_Ch1_PS {
+
+    public static void main(String[] args) {
+      // Question1
+        // int a = 5;
+        // int b = 10;
+        // int c = 15;
+        // int sum = a + b + c;
+        // System.out.println(sum);
+
+      // Question2
+        // float subject1 = 55;
+        // float subject2 = 75;
+        // float subject3 = 95;
+        // float cgpa = (subject1 + subject2 + subject3)/30;
+        // System.out.println(cgpa);
+
+       // Question3
+         // System.out.println("What Is Your Name");
+         // Scanner sc = new Scanner(System.in);
+         // String name = sc.next();
+         // System.out.println("Hello " + name + " Have A Good Day!");
+
+       // Question4
+         // double kilometers;
+         // System.out.println("Enter Kilometers : ");
+         // Scanner sc = new Scanner(System.in);
+         // kilometers = sc.nextDouble();
+         // double miles = kilometers / 1.6;
+         // System.out.println(miles + " Miles");
+
+       // Question5
+        System.out.println("Enter Your Number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+
+}
+
+
+
+
