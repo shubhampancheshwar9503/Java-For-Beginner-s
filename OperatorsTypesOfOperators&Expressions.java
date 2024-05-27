e40fb7455c700ff6aa2712108c96d1d50dc811bf
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-27 15:42:27 +0530

    Add files via upload

---
+++ OperatorsTypesOfOperators&Expressions.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class CWH_Ch2_Operators {
+
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+           int a = 5;
+        // int b = 10 % a; //Modulo Operator
+        // 4.8%1.1 -->Returns Decimal Remainder
+
+        // 2. Assignment Operators
+          int b = 10;
+          b *= 15;
+          System.out.println(b);
+
+        // 3. Comparison Operators
+        // System.out.println(55<5);
+
+        // 4. Logical Operators
+        // System.out.println(55>5 && 55>10);
+           System.out.println(55>5 || 55>10);
+
+        // 5. Bitwise Operators
+          System.out.println(2&3);
+          // 10
+          // 15
+        // ------
+          // 10
+    }
+    
+}
