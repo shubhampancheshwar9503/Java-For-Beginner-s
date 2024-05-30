d15407deee6d413d872dcc4b02a01b2fb319d506
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-05-30 22:05:19 +0530

    Add files via upload

---
+++ Chapter2PracticeSet.java
@@ -0,0 +1,42 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_Ch2_PS {
+
+    public static void main(String[] args) {
+      // Question1
+        // float a = 7 / 4.0f * 9 / 2.0f;
+        // System.out.println(a);
+
+      // Question2
+        // char grade = 'B';
+        // grade = (char)(grade + 8);
+        // System.out.println(grade);
+        // Decrypting The Grade
+        // grade = (char)(grade - 8);
+        // System.out.println(grade);
+
+       // Question3
+         // Scanner sc = new Scanner(System.in);
+         // int a = sc.nextInt();
+         // System.out.println(a > 8);
+
+       // Question4
+         // float v = 7;
+         // float u = 5;
+         // float a = 3;
+         // float s = 1;
+         // float b = (v * v  - u * u) / (2 * a * s);
+         // System.out.println(b);
+
+       // Question5
+        int x = 7;
+        int a = 7 * 49 / 7 + 35 / 7;
+        System.out.println(a);
+    }
+
+}
+
+
+
+
