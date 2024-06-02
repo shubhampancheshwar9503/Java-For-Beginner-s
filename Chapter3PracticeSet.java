0c9f55dc35b2e47665a0ab535e9c4b612dbbe87c
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-06-02 15:09:54 +0530

    Add files via upload

---
+++ Chapter3PracticeSet.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class CWH_Ch3_PS3 {
+
+    public static void main(String[] args) {
+        // Problem 1
+        // String name = "Shubham Pancheshwar";
+        // name = name.toLowerCase();
+        // System.out.println(name);
+
+        // Problem 2
+        // String text = "To My     Friend";
+        // text = text.replace(" ", "_");
+        // System.out.println(text);
+
+        // Problem 3
+        // String letter = "Dear <|name|>, Thanks a lot!";
+        // letter = letter.replace("<|name|>", "Shubham");
+        // System.out.println(letter);
+
+        // Problem 4
+        // String myString = "This string contains     double and triple spaces";
+        // System.out.println(myString.indexOf("   "));
+        // System.out.println(myString.indexOf("     "));
+
+        // Problem 5
+        String letter2 = "Dear Harry, \n\tThis Java Course is Nice.\n\tThanks!";
+        System.out.println(letter2);
+    }
+
+}
