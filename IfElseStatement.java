14211bfcbec7ce23b31f45e2c7627f89e1bb8944
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-06-03 13:28:52 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class CWH_16_Conditionals {
+
+    public static void main(String[] args) {
+        int age = 18;
+        if(age!=18){
+            System.out.println("Yes boy you can drive!");
+        }
+         else{
+             System.out.println("No boy you cannot drive yet!");
+         }
+
+    }
+
+}
