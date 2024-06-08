b20245323d34a88fec5a9ec03b4f42be5967123b
Shubham Pancheshwar <138433677+shubhampancheshwar9503@users.noreply.github.com>
2024-06-08 16:33:51 +0530

    Add files via upload

---
+++ RelationalAndLogicalOperators.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CWH_17_Logical {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        // if (a || b){
+            // System.out.println("Y");
+        // }
+        // else{
+            // System.out.println("N");
+        // }
+        System.out.println("For Logical OR...");
+
+        // if(a || b){
+            // System.out.println("Y");
+        // }
+        // else{
+            // System.out.println("N");
+        // }
+        System.out.println("For Logical NOT");
+        System.out.println("Not(A) is");
+        System.out.println(!a);
+        System.out.println("Not(B) is");
+        System.out.println(!b);
+    }
+
+}
